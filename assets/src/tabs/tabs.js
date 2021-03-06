8b272c3e4fc18f2d825dbb2541cfcd686230c84e
Awilum <sergey.romanenko@flextype.org>
2021-03-06 10:07:16 +0300

    feat(styles): delete styles for custom tabs

--- assets/src/tabs/tabs.js
+++
@@ -1,4 +0,0 @@
-$(".plugin-form-tabs__nav").on("click", ".plugin-form-tabs__nav__link:not(.plugin-form-tabs__nav__link--active)", function () {
-    $(this).addClass("plugin-form-tabs__nav__link--active").siblings().removeClass("plugin-form-tabs__nav__link--active");
-    $("div.plugin-form-tabs__content").removeClass("plugin-form-tabs__content--active").eq($(this).index()).addClass("plugin-form-tabs__content--active");
-});
